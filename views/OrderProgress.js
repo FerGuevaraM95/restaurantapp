8f2fcf65359a6b9103c660a381a0ea98aeaf9834
Fernando Guevara <luisfernandoguevara95@gmail.com>
2020-09-30 22:26:23 -0500

    (Add): Get delivery time

--- views/OrderProgress.js
+++ views/OrderProgress.js
@@ -3,14 +3,30 @@ import {View, StyleSheet} from 'react-native';
 import {Container, Text, H1, H3, Button} from 'native-base';
 import {useNavigation} from '@react-navigation/native';
 
+import {firebase} from '../firebase';
 import {OrdersContext} from '../context/orders/ordersContext';
 import globalStyles from '../styles/global';
 
 export const OrderProgress = () => {
   const {orderId} = useContext(OrdersContext);
+
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const getProduct = () => {
+      firebase.db
+        .collection('orders')
+        .doc(orderId)
+        .onSnapshot(function (doc) {
+          setTime(doc.data().deliveryTime);
+        });
+    };
+    getProduct();
+  }, []);
+
   return (
     <Container>
-      <Text>{orderId}</Text>
+      <Text>{time}</Text>
     </Container>
   );
 };
